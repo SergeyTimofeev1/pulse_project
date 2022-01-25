485aef575010fa4cff200b476a0a7a28b6f91810
Sergey Timofeev <Timofeevbest@yandex.ru>
2022-01-25 15:56:34 +0300

    create modal

--- src/js/script.js
+++ src/js/script.js
@@ -23,22 +23,6 @@ $(document).ready(function(){
     .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
 
-  // $('.catalog-item__link').each(function(i) {
-  //   $(this).on('click', function(e) {
-  //     e.preventDefault();
-  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
-  //   })
-  // });
-
-  // $('.catalog-item__back').each(function(i) {
-  //   $(this).on('click', function(e) {
-  //     e.preventDefault();
-  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
-  //   })
-  // });
-
   function toggleSlide(item) {
     $(item).each(function(i) {
       $(this).on('click', function(e) {
@@ -53,5 +37,19 @@ $(document).ready(function(){
   toggleSlide('.catalog-item__back');
 
 
+  // Modal
+
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+  })
+  $('.catalog-item__btn').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    })
+  })
 });
 	
